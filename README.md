55aea7d047bf9f577fce69b37d5cb434b02531b5
Joseph Furiate <furiatejoseph@yahoo.com>
2016-02-08 14:59:15 -0500

    Code Cleaning
    Made code a little more readable and added some comments.

---
+++ README.md
@@ -0,0 +1,3 @@
+Data Mining Assignment 1
+
+C++ program to do simple statistical analysis of provided data set.
--- src/assign1.cpp
+++ src/assign1.cpp
@@ -1,16 +1,23 @@
 // Author: Jospeh Furiate
 // FSUID:  jaf13j
+// Github: https://github.com/BackwardsHat
+// Summary: C++ program to calculate statistics of given data set. 
 
 #include <iostream>
 #include <fstream>
 #include <algorithm>	// sort()
-#include <math.h>		// ceil(), floor()
+#include <math.h>	// ceil(), floor()
 #include <iomanip>
 #include <vector>
 
 using namespace std;
 
+// Prototypes
 int readInput(const char *, vector<int>&, vector<int>&); 
+double calcQuartile(const vector<int>&, double); 
+double calcStdDev(const vector<int>&, double);
+void countOccurences(const vector<int>&, int *, int&); 
+void calcMinMaxMean(const vector<int>&, int&, int&, double&);
 void calcStatistics(const vector<int>&);
 
 int main(int argc, char * argv[]) {
@@ -63,26 +70,14 @@ int readInput(
 	return 0;
 }
 
-// Calculates 1st quartile
-double calcQ1(const vector<int>& scores) {
+// Calculates quartile based on given percentage
+double calcQuartile(const vector<int>& scores, double percentage) {
 	int n = scores.size();
 	int a, b;
 
 	// a and b will be the same value if n is odd
-	a = (floor((n-1) * .25));
-	b = (ceil((n-1) * .25));
-
-	return (scores[a] + scores[b]) / 2.0;
-}
-
-// Calculates 3rd quartile
-double calcQ3(const vector<int>& scores) {
-	int n = scores.size();
-	int a, b;
-
-	// a and b will be the same value if n is odd
-	a = (floor((n-1)* .75));
-	b = (ceil((n-1) * .75));
+	a = (floor((n-1) * percentage));
+	b = (ceil((n-1) * percentage));
 
 	return (scores[a] + scores[b]) / 2.0;
 }
@@ -104,53 +99,73 @@ double calcStdDev(const vector<int>& scores, double mean) {
 	// return sqrt(variance);
 }
 
-void calcStatistics(const vector<int>& scores) {
-	int max, min, n;	
-	double mean, std_dev, median, q1, q3; 
-	int buckets[102] = {0}; // Enough for 0 to 100  	
-	int maxModeCount = 0;
-	
-	n = scores.size(); // Score numbers
-
-	// Set to `impossible` values before calculating 
-	mean = 0;
-	min = 99999;
-	max = -99999;
-
-	// Quartiles
-	median = scores[(n-1)/2]; 
-	q1 = calcQ1(scores);
-	q3 = calcQ3(scores);
-
-	// Calculates min, max, and sums mean and fills mode buckets
+// Tallies scores occurences
+void countOccurences(
+		const vector<int>& scores,
+	   	int * buckets,
+		int& maxModeCount) {
 	for(vector<int>::const_iterator it = scores.begin();
 			it != scores.end(); ++it) {
-		if(*it > max) max = *it;
-		if(*it < min) min = *it;
-		mean += *it;
-
 		// Counts occurences of each score
 		buckets[*it]++;
 		// Tracks highest occurence of some score
 		if(maxModeCount < buckets[*it])
 		   	maxModeCount = buckets[*it];
 	}
+}
 
-	// Mean and standard deviation
-	mean /= n;
+
+// Calculates min, max, and sums mean and fills mode buckets
+void calcMinMaxMean(
+		const vector<int>& scores,
+	   	int& min,
+	   	int& max,
+	   	double& mean) {
+	for(vector<int>::const_iterator it = scores.begin();
+			it != scores.end(); ++it) {
+		if(*it > max) max = *it;
+		if(*it < min) min = *it;
+		mean += *it;
+
+		}
+
+	mean /= scores.size();
+}
+
+// Computes and outputs stats
+void calcStatistics(const vector<int>& scores) {
+	int max, min;	
+	double mean, std_dev, median, q1, q3; 
+	int buckets[102] = {0}; // Enough for 0 to 100  	
+	int maxModeCount = 0;
+
+	// Set to `impossible` values before calculating 
+	mean = 0;
+	min = 99999;
+	max = -99999;
+
+	// Calculate Quartiles
+	median = calcQuartile(scores, .50); 
+	q1 = calcQuartile(scores, .25);
+	q3 = calcQuartile(scores, .75);
+
+	// Calculate the rest
+	calcMinMaxMean(scores, min, max, mean);
 	std_dev = calcStdDev(scores, mean);
+	countOccurences(scores, buckets, maxModeCount);
 
+	// Output
 	cout << fixed;
 	cout << setprecision(3);
-	cout << "\nMax:\t\t" 	<< max 
-		 << "\nMin:\t\t" 	<< min
-		 << "\nMedian:\t\t"<< median
-		 << "\nMean:\t\t" 	<< mean
-		 << "\nStd Dev:\t"	<< std_dev
-		 << "\nQ1:\t\t"		<< q1
-		 << "\nQ3:\t\t"		<< q3;
+	cout << "\nMax:\t\t"	<< max 
+		 << "\nMin:\t\t"	<< min
+		 << "\nMedian:\t\t"	<< median
+		 << "\nQ1:\t\t"	<< q1
+		 << "\nQ3:\t\t"	<< q3;
 	
-	cout << "\nMode:\t\t";
+	cout << "\n\nMean:\t\t"	<< mean
+		 << "\nStd Dev:\t"	<< std_dev
+		 << "\nMode:\t\t";
 	// Outputs scores with the highest occurence 
 	for(int i = 0; i < 101; ++i)
 		if(buckets[i] == maxModeCount)
